e4034f4ceab33e77d5f4ac98fe3f8d8626057751
Sergei Gritsun <79272724+spgritsun@users.noreply.github.com>
2024-06-22 03:20:54 +0300

    Подключена база данный PostrgeSQL

--- MountainPass/settings.py
+++ MountainPass/settings.py
@@ -11,6 +11,9 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 """
 import os
 from pathlib import Path
+from dotenv import load_dotenv
+
+load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -73,13 +76,23 @@ WSGI_APPLICATION = 'MountainPass.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'MountainPass',
+        'USER': os.getenv('DB_USER'),
+        'PASSWORD': os.getenv('DB_PASSWORD'),
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
-
 # Password validation
 # https://docs.djangoproject.com/en/5.0/ref/settings/#auth-password-validators
 
--- drf/views.py
+++ drf/views.py
@@ -1,9 +1,6 @@
-from django.shortcuts import render
 
 # Create your views here.
 from rest_framework import viewsets
-from rest_framework.generics import ListAPIView
-
 from drf.serializers import PassSerializer
 from main.models import Pass
 
