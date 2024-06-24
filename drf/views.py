2504b0b815642bffbe89b7896ec478426c032045
Sergei Gritsun <79272724+spgritsun@users.noreply.github.com>
2024-06-24 16:02:48 +0300

    Реализован функционал отображения списков объектов пользователя по его email

--- drf/views.py
+++ drf/views.py
@@ -1,4 +1,3 @@
-
 # Create your views here.
 from rest_framework import viewsets
 from drf.serializers import PassSerializer
@@ -9,3 +8,10 @@ class PassViewSet(viewsets.ModelViewSet):
     queryset = Pass.objects.all()
     serializer_class = PassSerializer
 
+    def get_queryset(self):
+        queryset = self.queryset
+        email = self.request.query_params.get('email', None)
+        if email is not None:
+            email = email.rstrip('/')
+            queryset = Pass.objects.filter(user__email=email)
+        return queryset
